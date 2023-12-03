e776fcdbf07679a215be48e204c00d90e9b97cf1
Deniska,VOLKRAFT <volkraft9@gmail.com>
2023-12-03 12:50:21 +0300

    Feat: create functiong localStorage.render

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ const form = document.querySelector('#form');
 const inputAdd = form.querySelector('#inputAdd');
 const listNotes = document.querySelector('#listNotes');
 const inputSearch = document.querySelector('#inputSearch');
+const allNotes = JSON.parse(localStorage.getItem('DataBase')) || [];
 
 
 // Функции
-
+if (allNotes.length > 0){
+    renderStorage()
+}
 
 // Функция рендера
 function renderNote(e){
@@ -20,8 +23,11 @@ function renderNote(e){
             <button class="btn-action btn-action--remove">Remove</button>
         </li>
         `;
+        pushNote()
+        saveStorage()
         listNotes.insertAdjacentHTML('afterbegin', newNote);
         inputAdd.value = '';
+        console.log(allNotes);
     } 
     else {
         alertInput()
@@ -65,8 +71,51 @@ function alertInput(){
     inputAdd.style.border = '2px solid red'
 }
 
+function pushNote(){
+    if (allNotes){
+        allNotes.push(inputAdd.value)
+    }
+}
+
+function saveStorage(){
+    localStorage.setItem('DataBase', JSON.stringify(allNotes));
+}
+
+function renderStorage(){
+    allNotes.forEach((note) => {
+        const noteFromArray = 
+        `
+        <li class="list-notes__item">
+            <p class="list-notes__text">${note}</p>
+            <button class="btn-action btn-action--remove">Remove</button>
+        </li>
+        `;
+        listNotes.insertAdjacentHTML('afterbegin', noteFromArray);
+
+    })
+}
+
 // события
 form.addEventListener('submit', renderNote);
 inputAdd.addEventListener('input', inputTransperent);
 inputSearch.addEventListener('keyup', filterNotes);
 listNotes.addEventListener('click', removeNoteFromRender);
+
+
+// const user = {
+//     name: 'Мое',
+//     surname: 'МоЁ',
+//     age: 14,
+//     address: {
+//         country: 'Россия',
+//         city: 'Москва',
+//     },
+// }
+// const userJSON = JSON.stringify(user);
+// localStorage.setItem('Заголовок для примера', userJSON);
+
+// const resultFromStorage = localStorage.getItem('Заголовок для примера')
+// console.log(resultFromStorage)
+// const parseString = JSON.parse(resultFromStorage);
+// console.log(parseString);
+
